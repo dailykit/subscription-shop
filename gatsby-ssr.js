8d0c26f0e9f7a232503648b5b8c0949c50db644d
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-04-27 12:30:21 +0530

    fix: sort meta and style tags

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -20,11 +20,20 @@ export const onPreRenderHTML = ({
    getHeadComponents,
 }) => {
    const headComponents = getHeadComponents()
-   headComponents.forEach(head => {
-      if (head.props && head.props['data-react-helmet']) {
-         delete head.props['data-react-helmet']
+   headComponents.sort((a, b) => {
+      if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+         return 0
       }
+
+      if (a.type === 'style') {
+         return 1
+      } else if (b.type === 'style') {
+         return -1
+      }
+
+      return 0
    })
+
    replaceHeadComponents(headComponents)
 }
 
