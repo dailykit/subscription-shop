d799b2824e9ee14654ce4c6ef528dd2d5862e4d9
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-05-21 18:07:22 +0530

    feat: enable ssr for preview

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
 class MyDocument extends Document {
    static async getInitialProps(ctx) {
       const initialProps = await Document.getInitialProps(ctx)
@@ -8,7 +7,7 @@ class MyDocument extends Document {
 
    render() {
       return (
-         <Html>
+         <Html lang="en">
             <Head>
                <script src="/env-config.js"></script>
             </Head>
