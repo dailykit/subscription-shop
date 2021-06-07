8ce08ef398fa908cbc94595a769e909c16cd4552
PriyaBihani <priyabihani.official@gmail.com>
2021-06-07 09:48:40 +0530

    refactor: cooking cards

--- src/pages/subscription/recipes/index.js
+++ src/pages/subscription/recipes/index.js
@@ -91,17 +91,65 @@ const Recipe = () => {
             <h1 tw="py-4 text-2xl md:text-3xl tracking-wide text-teal-900">
                {recipe.name}
             </h1>
-            <div tw="grid grid-flow-col grid-cols-2 gap-4">
-               {!!recipe.description && (
+            <div tw="grid grid-flow-col gap-2">
+               <div tw="col-start-1 col-end-4">
+                  {!!recipe.description && (
+                     <div>
+                        <h2 tw="pb-2 mt-4 border-b border-gray-300 text-gray-600 text-lg font-normal mb-2">
+                           Description
+                        </h2>
+                        <p tw="text-teal-900">{recipe.description}</p>
+                     </div>
+                  )}
+                  {recipe.showIngredients && (
+                     <>
+                        <h2 tw="pb-2 mt-4 border-b border-gray-300 text-gray-600 text-lg font-normal mb-4">
+                           Ingredients
+                        </h2>
+                        <div tw="grid sm:grid-cols-3 grid-cols-2 gap-4">
+                           {productOption.simpleRecipeYield.sachets.map(
+                              ({ isVisible, slipName, sachet }, index) => (
+                                 <div
+                                    key={index}
+                                    css={[
+                                       tw`px-2 flex flex-col items-center w-48 mb-4`,
+                                       !isVisible && tw`justify-center`,
+                                    ]}
+                                 >
+                                    {isVisible ? (
+                                       <>
+                                          {sachet.ingredient.assets?.images
+                                             ?.length ? (
+                                             <img
+                                                src={
+                                                   sachet.ingredient.assets
+                                                      .images[0]
+                                                }
+                                                tw="w-24 h-24 rounded-full"
+                                             />
+                                          ) : (
+                                             <img
+                                                src={noProductImage}
+                                                tw="w-24 h-24 rounded-full"
+                                             />
+                                          )}
+                                          {renderIngredientName(
+                                             slipName,
+                                             sachet
+                                          )}
+                                       </>
+                                    ) : (
+                                       <LockIcon size="32" />
+                                    )}
+                                 </div>
+                              )
+                           )}
+                        </div>
+                     </>
+                  )}
+               </div>
+               <div tw="col-start-8">
                   <div>
-                     <h2 tw="pb-2 mt-4 border-b border-gray-300 text-gray-600 text-lg font-normal mb-2">
-                        Description
-                     </h2>
-                     <p tw="text-teal-900">{recipe.description}</p>
-                  </div>
-               )}
-               <div>
-                  <div tw="mb-2 grid grid-cols-2 gap-4">
                      <h2 tw="pb-2 mt-4 border-b border-gray-300 text-gray-600 text-lg font-normal mb-6">
                         Details
                      </h2>
@@ -111,32 +159,36 @@ const Recipe = () => {
                               <p tw="text-teal-900">{recipe.type}</p>
                            </div>
                         )} */}
-                     {!!recipe.cuisine && (
-                        <div tw="flex flex-col items-center">
-                           <CuisineIcon size={50} color={theme?.accent} />
-                           <p tw="text-teal-900">{recipe.cuisine}</p>
-                        </div>
-                     )}
-                     {!!recipe.author && (
-                        <div tw="flex flex-col items-center">
-                           <ChefIcon size={50} color={theme?.accent} />
-                           <p tw="text-teal-900">{recipe.author}</p>
-                        </div>
-                     )}
-                     {!!recipe.cookingTime && (
-                        <div tw="flex flex-col items-center">
-                           <TimeIcon size={50} color={theme?.accent} />
-                           <p tw="text-teal-900">{recipe.cookingTime} mins.</p>
-                        </div>
-                     )}
-                     {!!recipe.utensils?.length && (
-                        <div tw="flex flex-col items-center">
-                           <UtensilsIcon size={50} color={theme?.accent} />
-                           <p tw="text-teal-900">
-                              {recipe.utensils.join(', ')}
-                           </p>
-                        </div>
-                     )}
+                     <div tw="mb-2 grid grid-cols-2 gap-4">
+                        {!!recipe.cuisine && (
+                           <div tw="flex flex-col items-center">
+                              <CuisineIcon size={50} color={theme?.accent} />
+                              <p tw="text-teal-900">{recipe.cuisine}</p>
+                           </div>
+                        )}
+                        {!!recipe.author && (
+                           <div tw="flex flex-col items-center">
+                              <ChefIcon size={50} color={theme?.accent} />
+                              <p tw="text-teal-900">{recipe.author}</p>
+                           </div>
+                        )}
+                        {!!recipe.cookingTime && (
+                           <div tw="flex flex-col items-center">
+                              <TimeIcon size={50} color={theme?.accent} />
+                              <p tw="text-teal-900">
+                                 {recipe.cookingTime} mins.
+                              </p>
+                           </div>
+                        )}
+                        {!!recipe.utensils?.length && (
+                           <div tw="flex flex-col items-center">
+                              <UtensilsIcon size={50} color={theme?.accent} />
+                              <p tw="text-teal-900">
+                                 {recipe.utensils.join(', ')}
+                              </p>
+                           </div>
+                        )}
+                     </div>
                   </div>
                   {!!recipe.notIncluded?.length && (
                      <div tw="mb-2">
@@ -150,48 +202,7 @@ const Recipe = () => {
                   )}
                </div>
             </div>
-            {recipe.showIngredients && (
-               <>
-                  <h2 tw="pb-2 mt-4 border-b border-gray-300 text-gray-600 text-lg font-normal mb-4">
-                     Ingredients
-                  </h2>
-                  <div tw="grid sm:grid-cols-3 grid-cols-2 gap-4">
-                     {productOption.simpleRecipeYield.sachets.map(
-                        ({ isVisible, slipName, sachet }, index) => (
-                           <div
-                              key={index}
-                              css={[
-                                 tw`px-2 flex flex-col items-center w-48 mb-4`,
-                                 !isVisible && tw`justify-center`,
-                              ]}
-                           >
-                              {isVisible ? (
-                                 <>
-                                    {sachet.ingredient.assets?.images
-                                       ?.length ? (
-                                       <img
-                                          src={
-                                             sachet.ingredient.assets.images[0]
-                                          }
-                                          tw="w-24 h-24 rounded-full"
-                                       />
-                                    ) : (
-                                       <img
-                                          src={noProductImage}
-                                          tw="w-24 h-24 rounded-full"
-                                       />
-                                    )}
-                                    {renderIngredientName(slipName, sachet)}
-                                 </>
-                              ) : (
-                                 <LockIcon size="32" />
-                              )}
-                           </div>
-                        )
-                     )}
-                  </div>
-               </>
-            )}
+
             {recipe.showProcedures && (
                <>
                   <h2 tw="pb-2 mt-4 border-b border-gray-300 text-gray-500 mb-3 text-lg font-medium">
@@ -204,43 +215,57 @@ const Recipe = () => {
                               <span tw="text-lg font-medium text-gray-700">
                                  {set.title}
                               </span>
-                              {set.instructionSteps.map(step =>
-                                 step.isVisible ? (
-                                    <li
-                                       key={step.title}
-                                       tw="h-auto mb-4 ml-4 mt-2"
-                                    >
-                                       {step.title && (
-                                          <span tw="text-gray-800">
-                                             {step.title}
-                                          </span>
-                                       )}
-                                       <StepImage>
-                                          {step.assets.images.length > 0 && (
-                                             <img
-                                                src={step.assets.images[0].url}
-                                                alt={
-                                                   step.assets.images[0].title
-                                                }
-                                                title={
-                                                   step.assets.images[0].title
-                                                }
-                                             />
+                              <div tw="flex flex-wrap justify-evenly items-stretch">
+                                 {set.instructionSteps.map(step =>
+                                    step.isVisible ? (
+                                       <li
+                                          key={step.title}
+                                          tw=" mb-4 mx-2 mt-2 inline-block"
+                                          css={[`width: 30%`]}
+                                       >
+                                          <StepImage>
+                                             {step.assets.images.length > 0 ? (
+                                                <img
+                                                   src={
+                                                      step.assets.images[0].url
+                                                   }
+                                                   alt={
+                                                      step.assets.images[0]
+                                                         .title
+                                                   }
+                                                   title={
+                                                      step.assets.images[0]
+                                                         .title
+                                                   }
+                                                   tw="w-24 h-24 "
+                                                />
+                                             ) : (
+                                                <img
+                                                   src={noProductImage}
+                                                   tw="w-24 h-24 "
+                                                />
+                                             )}
+                                          </StepImage>
+                                          {step.title && (
+                                             <span tw="text-gray-800 ">
+                                                {step.title}
+                                             </span>
                                           )}
-                                       </StepImage>
-                                       <p tw="mt-1 text-gray-600">
-                                          {step.description}
-                                       </p>
-                                    </li>
-                                 ) : (
-                                    <li
-                                       key={step.title}
-                                       tw="h-auto mb-4 ml-4 mt-2"
-                                    >
-                                       <LockIcon />
-                                    </li>
-                                 )
-                              )}
+                                          <p tw="mt-1 text-gray-600">
+                                             {step.description}
+                                          </p>
+                                       </li>
+                                    ) : (
+                                       <li
+                                          key={step.title}
+                                          tw="h-auto mb-4 mx-2 mt-2 inline-block flex justify-center"
+                                          css={[`width: 30%`]}
+                                       >
+                                          <LockIcon size="50" />
+                                       </li>
+                                    )
+                                 )}
+                              </div>
                            </ol>
                         </li>
                      ))}
@@ -259,7 +284,7 @@ export default Recipe
 
 const RecipeContainer = styled.div`
    margin: auto;
-   max-width: 1000px;
+   max-width: 1090px;
    padding: 16px 0;
    width: calc(100vw - 40px);
 `
@@ -278,7 +303,7 @@ const StepImage = styled.div`
    img {
       width: 100%;
       height: 220px;
-      ${tw`object-cover rounded`}
+      ${tw`object-cover`}
       @media (max-width: 567px) {
          height: 160px;
       }
